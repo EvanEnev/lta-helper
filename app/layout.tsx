1e64e553bd3988248188ffa57f46ef2e591fe05c
EvanEnev <evanenev@outlook.com>
2024-09-05 02:33:45 +0300

    :lipstick: Change Select to Buttons

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout() {
       </head>
       <body className={inter.className}>
         <StateProvider>
-          <NextUIProvider>
+          <NextUIProvider locale="ru-RU">
             <AuthProvider />
           </NextUIProvider>
         </StateProvider>
--- src/components/schedule/DayInfo.tsx
+++ src/components/schedule/DayInfo.tsx
@@ -2,6 +2,7 @@ import daysState from '@/src/state/daysState'
 import selectedDayState from '@/src/state/selectedDayState'
 import {Day} from '@/src/utils/types'
 import {
+  Button,
   Card,
   CardBody,
   Input,
@@ -18,11 +19,11 @@ export default function DayInfo() {
 
   const day: Day = useMemo(
     () => days?.find(d => d.date === selectedDay.date) || {date: ''},
-    [days, selectedDay.date],
+    [days, selectedDay],
   )
 
-  const possibilityHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = event.target.value
+  const possibilityHandler = (value: string) => {
+    // const value = event.target.value
 
     if (value !== '-') setSelectedDay({...selectedDay, invalidComment: false})
     if (value === day?.value) return
@@ -48,23 +49,29 @@ export default function DayInfo() {
 
   return (
     <div className="w-full flex flex-col gap-4">
-      <Select
-        selectedKeys={[day?.value || '']}
-        required
-        onChange={possibilityHandler}
+      <Button
+        className="h-14"
         size="lg"
-        label="Возможность"
-        labelPlacement="inside">
-        <SelectItem key="+" className="h-[50px]">
-          Могу
-        </SelectItem>
-        <SelectItem key="-" className="h-[50px]">
-          Не могу
-        </SelectItem>
-        <SelectItem key="+/-" className="h-[50px]">
-          По договорённости
-        </SelectItem>
-      </Select>
+        color={day?.value === '+' ? 'success' : 'default'}
+        onClick={() => possibilityHandler('+')}>
+        Могу
+      </Button>
+
+      <Button
+        className="h-14"
+        size="lg"
+        color={day?.value === '-' ? 'danger' : 'default'}
+        onClick={() => possibilityHandler('-')}>
+        Не могу
+      </Button>
+
+      <Button
+        className="h-14"
+        size="lg"
+        color={day?.value === '+/-' ? 'warning' : 'default'}
+        onClick={() => possibilityHandler('+/-')}>
+        По договорённости
+      </Button>
       <Input
         label="Комментарий"
         size="lg"
