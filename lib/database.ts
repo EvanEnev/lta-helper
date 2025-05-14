2bd702d29fc332eeeec3957007833325014bfcc1
EvanEnev <evanenev@outlook.com>
2025-05-15 02:11:33 +0300

    Refactor database, move awayt from global

--- lib/database.ts
+++ lib/database.ts
@@ -1,16 +1,10 @@
 import {Pool} from 'pg'
 
-// @ts-ignore
-let db: Pool = global.pg
-
-if (!db) {
-  // @ts-ignore
-  db = global.pg = new Pool({
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    host: process.env.DATABASE_URL,
-    port: 5432,
-  })
-}
+const db = new Pool({
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_URL,
+  port: 5432,
+})
 
 export default db
