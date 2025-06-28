47c5a6107abf3962184d9bf27c1ceb726b39226f
EvanEnev <evanenev@outook.com>
2025-06-28 13:31:50 +0300

    Add getRanks function

---
+++ lib/functions/getRanks.ts
@@ -0,0 +1,24 @@
+import db from '@/lib/database'
+import {LTRank} from '@/src/utils/types'
+
+export default async function getRanks(): Promise<LTRank[]> {
+  const query = `SELECT
+  name,
+  salary,
+  overwork,
+  max_points,
+  max_shift_points,
+  weight
+  FROM lt_arena.ranks`
+
+  const result = await db.query(query)
+
+  return result.rows.map(row => ({
+    name: row.name,
+    salary: row.salary,
+    maxPoints: row.max_points,
+    maxShiftPoints: row.max_shift_points,
+    weight: row.weight,
+    overwork: row.overwork,
+  }))
+}
