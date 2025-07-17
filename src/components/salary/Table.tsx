c94b4db6ed030c16a22a51e43697470c8d0e88d0
EvanEnev <evanenev@outook.com>
2025-07-17 23:06:38 +0300

    Move table to a separated component

--- src/components/salary/Table.tsx
+++ src/components/salary/Table.tsx
@@ -1,14 +1,6 @@
 import {LTLocation, SalaryData, SalaryUser, UserSalary} from '@/src/utils/types'
-import {flexRender, getCoreRowModel, useReactTable} from '@tanstack/react-table'
-import {
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-  memo,
-  Fragment,
-} from 'react'
+import {Header} from '@tanstack/react-table'
+import {useCallback, useEffect, useMemo, useRef, useState, memo} from 'react'
 import Cell from './Cell'
 import {RowData} from '@tanstack/react-table'
 import WorkerCell from '@/src/components/salary/WorkerCell'
@@ -17,12 +9,12 @@ import {Socket} from 'socket.io-client'
 import {DateTime} from 'luxon'
 import {useAuth} from '@/src/components/global/providers/authProvider'
 import capitalize from '@/lib/functions/capitalize'
-import {Divider, Spinner} from '@heroui/react'
+import {Spinner} from '@heroui/react'
 import MonthSelect from '@/src/components/salary/MonthSelect'
-import TableRow from './TableRow'
 import LocationSelect from '@/src/components/salary/LocationSelect'
 import useIsMobile from '@/src/hooks/useIsMobile'
 import fetchHandler from '@/src/utils/global/fetchHandler'
+import CTable from '@/src/components/global/table/Table'
 
 declare module '@tanstack/react-table' {
   interface ColumnMeta<TData extends RowData, TValue> {
@@ -248,22 +240,6 @@ export default memo(function Table({
     return [...baseColumns, ...daysColumns]
   }, [showUserColumn, daysColumns])
 
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  })
-
-  const getFixedColumnLeftPosition = (fixedIndex: number = 0) => {
-    let leftOffset = 0
-    const columns = table.getAllColumns()
-    for (let i = 0; i < fixedIndex; i++) {
-      leftOffset += columns[i].getSize()
-    }
-
-    return leftOffset
-  }
-
   const onMonthUpdate = useCallback(
     async (date: string) => {
       setLoading(true)
@@ -304,6 +280,15 @@ export default memo(function Table({
     [date],
   )
 
+  const headerClassNameAction = useCallback(
+    (header: Header<any, any>) =>
+      header.column.columnDef.header ===
+      today.toFormat('EEE, dd.MM', {locale: 'ru-RU'})
+        ? 'bg-success-600 text-foreground-100'
+        : 'bg-default-100',
+    [today],
+  )
+
   return (
     <div className="h-full w-full px-2">
       <div
@@ -321,68 +306,11 @@ export default memo(function Table({
           </>
         )}
       </div>
-      <div className={`bg-content1 rounded-large relative w-full pt-4`}>
-        <table className="h-auto w-full max-w-full">
-          <thead
-            className="[&>tr]:first:shadow-small sticky z-1000"
-            style={{
-              top: `${document.querySelector('header')?.offsetHeight}px`,
-            }}>
-            {table.getHeaderGroups().map(headerGroup => (
-              <tr key={headerGroup.id} className="rounded-2xl">
-                {headerGroup.headers.map((header, index) => {
-                  return (
-                    <Fragment key={header.id}>
-                      <th
-                        key={header.id}
-                        className={`h-[2rem] w-[5rem] min-w-[5rem] px-2 py-3 align-middle text-xs font-medium tracking-wider first:rounded-s-lg last:rounded-e-lg ${header.column.columnDef.meta?.frozen ? 'bg-content2 sticky z-100' : ''} ${
-                          header.column.columnDef.header ===
-                          today.toFormat('EEE, dd.MM', {locale: 'ru-RU'})
-                            ? 'bg-success-600 text-foreground-100'
-                            : 'bg-default-100'
-                        }`}
-                        style={{
-                          width: `${header.getSize()}px`,
-                          minWidth: `${header.getSize()}px`,
-                          ...(header.column.columnDef.meta?.frozen && {
-                            left: `${getFixedColumnLeftPosition(
-                              header.column.columnDef.meta?.fixedPosition,
-                            )}px`,
-                          }),
-                        }}>
-                        {flexRender(
-                          header.column.columnDef.header,
-                          header.getContext(),
-                        )}
-                      </th>
-                      <th className="bg-default-100 w-px py-2">
-                        <Divider
-                          className="mx-auto h-[2rem]"
-                          orientation="vertical"
-                          hidden={index === headerGroup.headers.length - 1}
-                        />
-                      </th>
-                    </Fragment>
-                  )
-                })}
-              </tr>
-            ))}
-          </thead>
-          <tbody className="after:block">
-            <tr className="h-4"></tr>
-            {table.getRowModel().rows.map((row, rowIndex) => (
-              <TableRow
-                key={row.id}
-                row={row}
-                rowIndex={rowIndex}
-                getFixedColumnLeftPosition={getFixedColumnLeftPosition}
-                canEdit={canEdit}
-                dataLength={data.length}
-              />
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <CTable
+        data={data}
+        columns={columns}
+        headerClassNameAction={headerClassNameAction}
+      />
     </div>
   )
 })
