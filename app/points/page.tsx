ae88ed31b6a6a50b92c7cb8850a0d59f63ce20f4
EvanEnev <evanenev@outook.com>
2025-07-18 11:29:19 +0300

    Add Points component

--- app/points/page.tsx
+++ app/points/page.tsx
@@ -1,12 +1,13 @@
 // import getPoints from '@/lib/functions/points/getPoints'
 // import {DateTime, Interval} from 'luxon'
-//
-// export default async function Points() {
-//   const now = DateTime.now()
-//   const interval = Interval.fromDateTimes(
-//     now.startOf('month'),
-//     now.endOf('month'),
-//   )
-//
-//   const pointsData = await getPoints({interval})
-// }
+
+export default async function Points() {
+  // const now = DateTime.now()
+  // const interval = Interval.fromDateTimes(
+  //   now.startOf('month'),
+  //   now.endOf('month'),
+  // )
+  //
+  // const pointsData = await getPoints({interval})
+  return <main></main>
+}
