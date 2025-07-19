83059eac65d191ab5ef15906b42b3199918e67d7
EvanEnev <evanenev@outook.com>
2025-07-20 01:55:42 +0300

    Remove unused useEffect

--- src/components/page/MainPage.tsx
+++ src/components/page/MainPage.tsx
@@ -4,26 +4,9 @@ import useIsMobile from '@/src/hooks/useIsMobile'
 import MobilePage from '@/src/components/page/MobilePage'
 import DesktopPage from '@/src/components/page/DesktopPage'
 import {ShortSalary} from '@/app/page'
-import {useAuth} from '@/src/components/global/providers/authProvider'
-import {Button} from '@heroui/react'
-import {useEffect} from 'react'
 
 export default function MainPage({salaryData}: {salaryData: ShortSalary}) {
   const isMobile = useIsMobile()
-  const {setPageSettings} = useAuth()
-
-  useEffect(() => {
-    setPageSettings([
-      {
-        label: 'test',
-        components: [
-          <div>
-            <Button></Button>
-          </div>,
-        ],
-      },
-    ])
-  }, [])
 
   return isMobile ? (
     <MobilePage salaryData={salaryData} />
