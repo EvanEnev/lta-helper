44af0084aa3b6207fe9679c2f492e80099c30a63
EvanEnev <evanenev@outook.com>
2025-08-26 10:28:04 +0300

    Add payrolls cards

--- src/components/payrolls/PayrollCard.tsx
+++ src/components/payrolls/PayrollCard.tsx
@@ -1,29 +1,59 @@
 import {LTPayroll} from '@/src/utils/types'
 import {DateTime, Interval} from 'luxon'
-import {Button, Card, CardBody, CardFooter, CardHeader} from '@heroui/react'
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Code,
+  semanticColors,
+} from '@heroui/react'
 import Link from 'next/link'
-import {Pen2} from 'solar-icon-set'
+import {CheckCircle, CloseCircle, Pen2} from 'solar-icon-set'
+import {useTheme} from 'next-themes'
 
 interface PayrollCardProps {
   data: LTPayroll
 }
 
 export default function PayrollCard({data}: PayrollCardProps) {
+  const {theme} = useTheme()
   const interval = Interval.fromISO(data.dates)
   const createdAt = DateTime.fromISO(data.createdAt)
   const takeBy = DateTime.fromISO(data.takeBy)
+  // @ts-ignore
+  const themeColors = semanticColors[theme || 'dark']
 
   return (
-    <Card className="h-[15rem] w-[15rem]">
+    <Card className="h-[18rem] w-[20rem]">
       <CardHeader>{interval.toFormat('dd.MM.yyyy')}</CardHeader>
-      <CardBody>
+      <CardBody className="flex flex-col gap-2">
         <p>
           Создана: {createdAt.toFormat('dd.MM.yyyy HH:mm')},{' '}
           {data.createdBy.name}
         </p>
         <p>Кол-во сотрудников: {data.workersCount}</p>
-        <p>Можно забрать до: {takeBy.toFormat('dd.MM.yyyy')}</p>
-        <p>Бонусы: {data.bonuses ? 'Да' : 'Нет'}</p>
+        <div>
+          <span>Можно забрать до: </span>
+          <Code color="success">{takeBy.toFormat('dd.MM.yyyy')}</Code>
+        </div>
+        <div className="flex items-center gap-2">
+          <p>Бонусы: </p>
+          {data.bonuses ? (
+            <CheckCircle
+              iconStyle="Bold"
+              color={themeColors.success['500']}
+              size={20}
+            />
+          ) : (
+            <CloseCircle
+              iconStyle="Bold"
+              size={20}
+              color={themeColors.danger['500']}
+            />
+          )}
+        </div>
       </CardBody>
       <CardFooter>
         <Button
--- src/components/payrolls/PayrollsCreateCard.tsx
+++ src/components/payrolls/PayrollsCreateCard.tsx
@@ -1,10 +1,109 @@
-import {Button} from '@heroui/react'
+import {
+  Button,
+  CalendarDate,
+  DateRangePicker,
+  useDisclosure,
+} from '@heroui/react'
 import {AddCircle} from 'solar-icon-set'
+import {
+  Modal,
+  ModalContent,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+} from '@heroui/modal'
+import {DateTime} from 'luxon'
+import {useMemo} from 'react'
+import {parseDate} from '@internationalized/date'
 
 export default function PayrollCreateCard() {
+  const {isOpen, onOpen, onOpenChange} = useDisclosure()
+
+  const currentDate = useMemo(() => DateTime.now().setZone('Europe/Moscow'), [])
+
+  const initialState: {start?: CalendarDate; end?: CalendarDate} = {
+    start: undefined,
+    end: undefined,
+  }
+
+  if (currentDate.day > 20 || currentDate.day < 5) {
+    const monthNumber =
+      currentDate.day < 5 ? currentDate.month - 1 : currentDate.month
+
+    initialState.start = parseDate(
+      currentDate
+        .set({
+          day: 15,
+          month: monthNumber,
+        })
+        .toFormat('yyyy-MM-dd'),
+    )
+
+    initialState.end = parseDate(
+      currentDate
+        .set({
+          day: currentDate.set({month: monthNumber}).daysInMonth,
+          month: monthNumber,
+        })
+        .toFormat('yyyy-MM-dd'),
+    )
+  } else {
+    const monthNumber = currentDate.day
+
+    initialState.start = parseDate(
+      currentDate
+        .set({
+          day: 1,
+          month: monthNumber,
+        })
+        .toFormat('yyyy-MM-dd'),
+    )
+
+    initialState.end = parseDate(
+      currentDate
+        .set({
+          day: currentDate.set({month: monthNumber}).daysInMonth,
+          month: monthNumber,
+        })
+        .toFormat('yyyy-MM-dd'),
+    )
+  }
+
   return (
-    <Button variant="ghost" className="w-full">
-      <AddCircle />
-    </Button>
+    <>
+      <Button
+        variant="ghost"
+        onPress={onOpen}
+        className="flex h-[18rem] w-[20rem] flex-col gap-2 border-dashed text-2xl">
+        <AddCircle size={50} />
+        Создать
+      </Button>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {onClose => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Создание ведомости
+              </ModalHeader>
+              <ModalBody>
+                <DateRangePicker
+                  defaultValue={initialState}
+                  label="Диапазон дат"
+                  labelPlacement="outside"
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
