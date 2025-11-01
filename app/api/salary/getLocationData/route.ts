df174836dd3b01a1d9ecdaf164aa8d21f4499ba5
EvanEnev <evanenev@outook.com>
2025-11-01 13:47:57 +0300

    Add location to a specific user

--- app/api/salary/getLocationData/route.ts
+++ app/api/salary/getLocationData/route.ts
@@ -53,7 +53,7 @@ export async function POST(req: NextRequest) {
   FROM lt_arena.salary s
   LEFT JOIN lt_arena.locations l ON s.location_id = l.id
   LEFT JOIN lt_arena.workers w ON s.worker_id = w.id
-  WHERE s.date = '${date.toFormat('yyyy-MM-dd')}' ${locationId ? `AND (s.location_id = ${locationId} OR s.location_id = 12 OR s.location_id = 13)` : ''}`
+  WHERE s.date = '${date.toFormat('yyyy-MM-dd')}' ${locationId ? `AND (s.location_id = ${locationId} OR s.location_id = 12${user.id === 42 ? ' or s.location_id = 17' : ''})` : ''}`
 
   const result = await db.query(query)
   const rows = result.rows
