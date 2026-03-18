0b111828f6425263ff4ba024debeda7effe7817c
EvanEnev <evanenev@outook.com>
2026-03-18 13:08:44 +0300

    Make always ask google account

--- lib/auth.ts
+++ lib/auth.ts
@@ -54,6 +54,7 @@ export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
   socialProviders: {
     google: {
+      prompt: 'select_account',
       clientId: process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
